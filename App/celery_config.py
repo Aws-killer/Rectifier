7c87ea86847e72b92e044a89ee5cbbe7b430bf68
Aws_killer <telegramsuper6@gmail.com>
2023-11-15 17:46:33 +0300

    chaneged broker

--- App/celery_config.py
+++ App/celery_config.py
@@ -7,7 +7,7 @@ accept_content = ["json"]
 timezone = "Europe/Oslo"
 enable_utc = True
 
-broker_url = f"rediss://default:8a715dd4651c8739b157d0ee0bb2d924@master.cache--j5zxzwppzvjs.addon.code.run:6379?ssl_cert_reqs=none"
+broker_url = f"rediss://default:157885c870c9fe1154ac9ae5d0ab97b6@master.llm-cache--6h5xnbs2d8rh.addon.code.run:6379?ssl_cert_reqs=none"
 result_backend = f"db+postgresql+psycopg2://postgres:PkkneZrSFsnJR6B@db.vfhoydxvxuesxhrcdnmx.supabase.co:5432/postgres"
 
 # SSL/TLS and SNI configuration
