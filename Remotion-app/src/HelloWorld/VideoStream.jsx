f4f68eba5da20c73713f5c137b64a2af27a52823
Aws_killer <telegramsuper6@gmail.com>
2023-11-16 23:28:18 +0300

    Sequence instead of series and transitions

--- Remotion-app/src/HelloWorld/VideoStream.jsx
+++ Remotion-app/src/HelloWorld/VideoStream.jsx
@@ -1,12 +1,12 @@
 import {Series} from 'remotion';
 import React from 'react';
-import {Video, staticFile, useVideoConfig} from 'remotion';
+import {Video, staticFile, useVideoConfig, Sequence} from 'remotion';
 import videoSequences from './Assets/VideoSequences.json';
 import {TransitionSeries} from '@remotion/transitions';
 export default function VideoStream() {
 	const {fps} = useVideoConfig();
 	return (
-		<TransitionSeries
+		<div
 			style={{
 				color: 'white',
 				position: 'absolute',
@@ -15,15 +15,15 @@ export default function VideoStream() {
 		>
 			{videoSequences.map((entry, index) => {
 				return (
-					<TransitionSeries.Sequence
+					<Sequence
 						key={index}
 						from={fps * entry.start}
 						durationInFrames={fps * (entry.end - entry.start)}
 					>
 						<Video {...entry.props} src={staticFile(entry.name)} />
-					</TransitionSeries.Sequence>
+					</Sequence>
 				);
 			})}
-		</TransitionSeries>
+		</div>
 	);
 }
