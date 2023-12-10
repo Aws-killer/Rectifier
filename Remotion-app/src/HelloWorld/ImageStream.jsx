308f422005c33514ff438050a27882ed66001a28
Aws_killer <telegramsuper6@gmail.com>
2023-12-10 11:02:21 +0300

    image stream rollbacl

--- Remotion-app/src/HelloWorld/ImageStream.jsx
+++ Remotion-app/src/HelloWorld/ImageStream.jsx
@@ -14,19 +14,13 @@ export default function ImageStream() {
 		>
 			{imageSequences.map((entry, index) => {
 				return (
-					<>
-						<TransitionSeries.Sequence
-							key={index}
-							from={fps * entry.start}
-							durationInFrames={fps * (entry.end - entry.start)}
-						>
-							<Img src={staticFile(entry.name)} />
-						</TransitionSeries.Sequence>
-						<TransitionSeries.Transition
-							presentation={slide()}
-							timing={linearTiming({durationInFrames: 1})}
-						/>
-					</>
+					<TransitionSeries.Sequence
+						key={index}
+						from={fps * entry.start}
+						durationInFrames={fps * (entry.end - entry.start)}
+					>
+						<Img src={staticFile(entry.name)} />
+					</TransitionSeries.Sequence>
 				);
 			})}
 		</TransitionSeries>
