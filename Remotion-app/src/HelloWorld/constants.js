87e03b8e456f29002e76a728b42df530b0d2e729
Aws_killer <telegramsuper6@gmail.com>
2023-12-11 09:02:50 +0300

    ok

--- Remotion-app/src/HelloWorld/constants.js
+++ Remotion-app/src/HelloWorld/constants.js
@@ -3,6 +3,6 @@
 export const COLOR_1 = '#86A8E7';
 export const COLOR_2 = '#91EAE4';
 
-export const FONT_FAMILY = 'AkayaKanadaka';
+export const FONT_FAMILY = 'Pacifico';
 export const FPS = 30;
 export const DURATION = 70 * FPS; // 30 seconds
