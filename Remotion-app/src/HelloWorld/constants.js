750ecb31b2d045ef389b6498e27bc72f36e8e581
Aws_killer <telegramsuper6@gmail.com>
2023-12-20 01:19:12 +0300

    Luckiest Guy

--- Remotion-app/src/HelloWorld/constants.js
+++ Remotion-app/src/HelloWorld/constants.js
@@ -1,6 +1,6 @@
 // Change any of these to update your video live.
 
-export const FONT_FAMILY = 'LuckiestGuy';
+export const FONT_FAMILY = 'Luckiest Guy';
 export const FPS = 30;
 
 export const DURATION = 70 * FPS; // 30 seconds
