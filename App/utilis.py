c61f05ad927a59d608805c53a4e57a9f7da8da7e
Aws_killer <telegramsuper6@gmail.com>
2024-02-26 18:24:37 +0300

    ffmpeg concat fix

--- App/utilis.py
+++ App/utilis.py
@@ -7,13 +7,13 @@ async def upload_file(file_path: str, node: str, chunk: int, task: str):
     master_node = SERVER_STATE.get_master()
     url = f"{master_node.SPACE_HOST}/uploadfile/?node={node}&chunk={chunk}&task={task}"
     async with aiohttp.ClientSession() as session:
-        # headers = {"Transfer-Encoding": "chunked"}
-        with open(file_path, "rb") as file:
-            async with session.post(url, data=file) as response:
-                if response.status == 200:
-                    print("File uploaded successfully")
-                else:
-                    print("Failed to upload file")
+        data = aiohttp.FormData()
+        data.add_field("file", open(file_path, "rb"), content_type="video/mp4")
+        async with session.post(url, data=data) as response:
+            if response.status == 200:
+                print("File uploaded successfully")
+            else:
+                print("Failed to upload file")
 
 
 class WorkerClient:
