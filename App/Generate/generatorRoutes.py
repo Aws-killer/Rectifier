3dfef62f63027b95a6d13f0e29251f941a252f3b
Aws_killer <telegramsuper6@gmail.com>
2024-05-15 01:27:06 +0300

    await models.create_all()

--- App/Generate/generatorRoutes.py
+++ App/Generate/generatorRoutes.py
@@ -18,7 +18,7 @@ async def main(request: GeneratorRequest):
     topic = request.prompt
     renderr = RenderVideo()
 
-    await models.create_all(database_url)
+    await models.create_all()
     message = chatbot(Prompt.format(topic=topic))
 
     generated_story = Story.from_dict(message["scenes"])
