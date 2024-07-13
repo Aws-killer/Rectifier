08cf236a600db371ccc08f8f597dbf934e0acc20
Aws_killer <telegramsuper6@gmail.com>
2024-07-13 06:11:08 +0300

    noo limits

--- Remotion-app/remotion.config.js
+++ Remotion-app/remotion.config.js
@@ -12,4 +12,4 @@ Config.overrideWebpackConfig((currentConfiguration) => {
 
 //Config.setBrowserExecutable("/usr/bin/chrome-headless-shell");
 Config.setVideoImageFormat('jpeg');
-Config.setConcurrency(5);
+// Config.setConcurrency(5);
