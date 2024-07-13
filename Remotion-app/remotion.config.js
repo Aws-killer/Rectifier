d253acc18b737bd0ec14135a65364dbe86b648cb
Aws_killer <telegramsuper6@gmail.com>
2024-07-13 15:55:00 +0300

    remotionconfig

--- Remotion-app/remotion.config.js
+++ Remotion-app/remotion.config.js
@@ -12,4 +12,4 @@ Config.overrideWebpackConfig((currentConfiguration) => {
 
 //Config.setBrowserExecutable("/usr/bin/chrome-headless-shell");
 Config.setVideoImageFormat('jpeg');
-Config.setConcurrency(64);
+// Config.setConcurrency(64);
